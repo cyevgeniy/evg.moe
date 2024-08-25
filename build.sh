75a8f9b7f6333d5bc6667748db78467dfb87af60
Evgeny Chaban <hi@evg.moe>
2024-08-25 13:51:45 +0300

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 
 deploy() {
-  rsync -vzr --delete -e 'ssh -i ~/.ssh/id_ed25519' public/  proj11co@93.125.99.59:public_html
-  echo "Deployed to https://proj11.com"
+  rsync -vzr --delete -e 'ssh -i ~/.ssh/id_ed25519' public/  evgmoe@93.125.99.58:public_html
+  echo "Deployed to https://evg.moe"
 }
 
 deploy
